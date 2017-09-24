1ac87c2f7ec6c906f36d840d7c275f9d847bf796
upandey3 <utki.nba@gmail.com>
2017-09-24 17:14:28 -0500

    Added text files for mazes with multiple dots

--- Search.py
+++ Search.py
@@ -2,9 +2,9 @@ import sys
 
 if len(sys.argv) < 2:
     raise Exception("Need two arguments: script.py maze.txt")
-
+print(sys.argv)
 maze_fname = sys.argv[1]
 
 with open(maze_fname) as mFile:
-    maze = mFile.read()
+    maze = mFile.read().strip()
 print(maze)
