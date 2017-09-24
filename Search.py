9b4be8022648352b3a35d4eaa903326c14d489e4
upandey3 <utki.nba@gmail.com>
2017-09-24 17:54:15 -0500

    Added getDosPositions function

--- Search.py
+++ Search.py
@@ -1,19 +1,18 @@
 import sys
 
-if len(sys.argv) < 2:
-    raise Exception("Need two arguments: script.py maze.txt")
-print(sys.argv)
-maze_fname = sys.argv[1]
+# Analyzes a new maze to return the row,col positions of all the dots
+def getDotPositions(grid):
+    return [(j, i) for j, r in enumerate(grid) for i, c in enumerate(r) if c == '.']
+
+# if len(sys.argv) < 2:
+#     raise Exception("Need two arguments: script.py maze.txt")
+# print(sys.argv)
+# maze_fname = sys.argv[1]
+maze_fname = "MediumMaze.txt"
+maze_fname = "BigMaze.txt"
+maze_fname = "TinySearch.txt"
 
 with open(maze_fname) as mFile:
     grid = mFile.readlines() # 2D Matrix
-    # print(grid[1])
-    # grid =
-    # for row in rows:
-    #     print(row)
-    # # for lines
-# print(maze)
-print(grid)
 
-catch IOError as instance:
-    print("Unable to open file" + instance.filename)
+print(getDotPositions(grid))
