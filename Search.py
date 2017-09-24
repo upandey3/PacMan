907b6e88e971375d2dbc423ef3e9c548df8db0a5
upandey3 <utki.nba@gmail.com>
2017-09-24 17:33:44 -0500

    Added 2D matrix : grid

--- Search.py
+++ Search.py
@@ -6,5 +6,14 @@ print(sys.argv)
 maze_fname = sys.argv[1]
 
 with open(maze_fname) as mFile:
-    maze = mFile.read().strip()
-print(maze)
+    grid = mFile.readlines() # 2D Matrix
+    # print(grid[1])
+    # grid =
+    # for row in rows:
+    #     print(row)
+    # # for lines
+# print(maze)
+print(grid)
+
+catch IOError as instance:
+    print("Unable to open file" + instance.filename)
