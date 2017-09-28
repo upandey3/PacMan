23ff3223f716da45fe2f8053203f75ea609d2d69
upandey3 <utki.nba@gmail.com>
2017-09-28 18:29:13 -0500

    Added Path cost. Cleaned up, commented code.

--- Search.py
+++ Search.py
@@ -1,82 +1,101 @@
 import sys
 from queue import *
-# Analyzes a new maze to return the row,col positions of all the dots
-def getDotPositions(grid):
-    return {(j, i) for j, r in enumerate(grid) for i, c in enumerate(r) if c == '.'}
 
-def getStartPosition(grid):
-    return [(j, i) for j, r in enumerate(grid) for i, c in enumerate(r) if c == 'P']
+class MazeSearch:
 
-def getValidNeighbors(grid, pos, explored, frontierList):
-    # assert (type(position) == 'tuple')
-    x = pos[1]
-    y = pos[0]
-    l = []
-    # for row in grid:
-    #     print(row)
-    up = (y-1, x)
-    right = (y, x+1)
-    down = (y+1, x)
-    left = (y, x-1)
+    # Constructor
+    def __init__(self, grid):
+        # Initialize the internal variables
+        self.grid = grid       # Intialize the maze grid
+        self.frontierSet = {}  # Creating a set of all the nodes that are in the frontier for finding purposes
+        self.explored = set()  # Set of all the nodes that have been already explored
+        self.dotPositions = self.getDotPositions()          # Get a set of tuples of dot positions
+        self.dotCount = len(self.dotPositions)              # Numbers of dots to find (used for goal test)
+        self.start = self.getStartPosition()                # Start position as a tuple
 
-    if (grid[up[0]][up[1]] != '%' and up not in explored and up not in frontierList): # Upper
-        l.append((y-1, x))
-    if (grid[right[0]][right[1]] != '%' and right not in explored and right not in frontierList): # Right
-        l.append((y, x+1))
-    if (grid[down[0]][down[1]] != '%' and down not in explored and down not in frontierList): # Down
-        l.append((y+1, x))
-    if (grid[left[0]][left[1]] != '%' and left not in explored and left not in frontierList): # Left
-        l.append((y, x-1))
-    return l
+    # Given a maze, returns a hashset of tuples - (row, col) positions of all the dots
+    def getDotPositions(self):
+        return {(j, i) for j, r in enumerate(self.grid) for i, c in enumerate(r) if c == '.'}
 
-# Returns a grid
-def graphSearch(grid, bfs = True):
-    dotPositions = getDotPositions(grid)
-    dotCount = len(dotPositions)
-    # print(dotCount)
-    start = getStartPosition(grid)
-    frontier = Queue(maxsize=0) if(bfs) else LifoQueue(maxsize=0)
-    frontier.put(start[0])
-    frontierList = [start[0]]
-    explored = set()
+    # Given a maze, returns the start point identified by 'P' as a tuple - (row, col)
+    def getStartPosition(self):
+        for j, r in enumerate(self.grid): # Get the idx, row(string) from grid
+            for i, c in enumerate(r): # Get the idx, character from the string
+                if c == 'P':
+                    return (j, i)
+        raise Exception("Could not find the start position - \'P\'")
 
-    while not frontier.empty():
-        node = frontier.get()
-        explored.add(node)
-        frontierList.remove(node)
-        if node in dotPositions:
-            dotCount -= 1 #decrease
+    # Check whether a node has not been visited yet
+    def isUnexplored(self, node):
+        return node not in self.explored and node not in self.frontierSet
 
-            if dotCount == 0: # Goal test
-                return grid
-        x = node[1]
-        y = node[0]
-        row = grid[y]
-        if grid[y][x] != 'P':
-            grid[y] = row[:x] + '.' + row[x+1:]
-        # row = grid[y]
-        neighbors = getValidNeighbors(grid, node, explored, frontierList)
-        for n in neighbors:
-            frontier.put(n)
-            frontierList.append(n)
+    # Gets a list of all the valid neighboring nodes that current node can move to
+    def getValidNeighbors(self, pos):
+        assert (isinstance(pos, tuple)) # Making sure pos is a type
+        l = [] # Initialize the list
+        x = pos[1]; y = pos[0];
+        up = (y-1, x)
+        right = (y, x+1)
+        down = (y+1, x)
+        left = (y, x-1)
+        # Check if the surrounding nodes are not walls and are unexplored
+        if (self.grid[up[0]][up[1]] != '%' and self.isUnexplored(up)): # Upper
+            l.append((y-1, x))
+        if (self.grid[right[0]][right[1]] != '%' and self.isUnexplored(right)): # Right
+            l.append((y, x+1))
+        if (self.grid[down[0]][down[1]] != '%' and self.isUnexplored(down)): # Down
+            l.append((y+1, x))
+        if (self.grid[left[0]][left[1]] != '%' and self.isUnexplored(left)): # Left
+            l.append((y, x-1))
+        return l
 
-# if len(sys.argv) < 2:
-#     raise Exception("Need two arguments: script.py maze.txt")
-# print(sys.argv)
-# maze_fname = sys.argv[1]
-maze_fname = "MediumMaze.txt"
+    # Solves a maze based on a given algorithm and returns (grid, path cost)
+    def solve(self, bfs = True):
+
+        # Intialize the data structure depending on the algorithm
+        frontier = Queue(maxsize=0) if(bfs) else LifoQueue(maxsize=0)
+        frontier.put((self.start, 0))  # Frontier: ((x, y), path cost) : tuple(tuple(x, y), int)
+        self.frontierSet = {self.start}# Creating a set of all the nodes that are in the frontier for finding purposes
+        self.explored = set()          # Set of all the nodes that have been already explored
+
+        while not frontier.empty():
+            t = frontier.get()      # Remove from frontier
+            node = t[0]; cost = t[1]
+            self.frontierSet.remove(node)
+            self.explored.add(node)    # Add to Explored set
+            if node in self.dotPositions:
+                self.dotCount -= 1     # If a dot position is found, decrement the count
+                if self.dotCount == 0: # Goal test - if all the dot positions have been found
+                    return self.grid, cost
+            x = node[1]; y = node[0]; row = self.grid[y] # Get coordinates an corresponding row
+            if self.grid[y][x] != 'P':                   # Don't replace the 'P'
+                self.grid[y] = row[:x] + '.' + row[x+1:] # Mark the visted node with a '.'
+            neighbors = self.getValidNeighbors(node)
+            for n in neighbors:        # Put all the valid neighbors in the frontier
+                frontier.put((n, cost+1))        # Put in the incremented cost
+                self.frontierSet.add(n)
+
+if len(sys.argv) < 2:
+    raise Exception("Need two arguments: script.py maze.txt")
+maze_fname = sys.argv[1]
+
+# maze_fname = "TestMaze.txt"
+# maze_fname = "MediumMaze.txt"
 # maze_fname = "BigMaze.txt"
 # maze_fname = "TinySearch.txt"
 
 with open(maze_fname) as mFile:
-    grid = mFile.readlines() # 2D Matrix
+    grid = mFile.readlines() # 2D Matrix of rows(string per line) and cols(character per string)
+
 # for row in grid:
 #     print(row)
 # print(grid)
 
-# a = Queue(maxsize = 0)
-# b = LifoQueue(maxsize = 0)
-# startPosition = getStartPosition(grid)
-# print(startPosition)
-newMaze = ''.join(graphSearch(grid, False))
+maze = MazeSearch(grid)
+newGrid, cost = maze.solve(True)
+newMaze = ''.join(newGrid)
 print(newMaze)
+print ("The path cost is "+ str(cost) + " steps")
+
+with open("output.text", "w") as oFile:
+    oFile.write(newMaze)
