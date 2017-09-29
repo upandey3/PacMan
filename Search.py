b9cdcb6d258529ffe873a40116cc76e52ca6df9b
upandey3 <utki.nba@gmail.com>
2017-09-28 21:52:55 -0500

    Added a function forManhattan Distance

--- Search.py
+++ Search.py
@@ -25,6 +25,13 @@ class MazeSearch:
                     return (j, i)
         raise Exception("Could not find the start position - \'P\'")
 
+    # Gets the Manhattan Distance between two coordinates
+    def mDistance(self, node1, node2):
+        assert(isinstance(node1, tuple) and isinstance(node2, tuple))
+        x1 = node1[1]; y1 = node1[0]
+        x2 = node2[1]; y2 = node2[0]
+        return abs(x2 - x1) + abs(y2 - y1)
+
     # Check whether a node has not been visited yet
     def isUnexplored(self, node):
         return node not in self.explored and node not in self.frontierSet
@@ -32,8 +39,8 @@ class MazeSearch:
     # Gets a list of all the valid neighboring nodes that current node can move to
     def getValidNeighbors(self, pos):
         assert (isinstance(pos, tuple)) # Making sure pos is a type
-        l = [] # Initialize the list
-        x = pos[1]; y = pos[0];
+        l = []     # Initialize the list
+        y, x = pos # x = pos[1]; y = pos[0];
         up = (y-1, x)
         right = (y, x+1)
         down = (y+1, x)
@@ -54,38 +61,38 @@ class MazeSearch:
 
         # Intialize the data structure depending on the algorithm
         frontier = Queue(maxsize=0) if(bfs) else LifoQueue(maxsize=0)
-        frontier.put((self.start, 0))  # Frontier: ((x, y), path cost) : tuple(tuple(x, y), int)
-        self.frontierSet = {self.start}# Creating a set of all the nodes that are in the frontier for finding purposes
-        self.explored = set()          # Set of all the nodes that have been already explored
+        frontier.put((self.start, 0))   # Frontier: ((x, y), path cost) : tuple(tuple(x, y), int)
+        self.frontierSet = {self.start} # Creating a set of all the nodes that are in the frontier for finding purposes
+        self.explored = set()           # Set of all the nodes that have been already explored
 
         while not frontier.empty():
             t = frontier.get()      # Remove from frontier
 
-            ###### Visualize the maze solving ######
-            time.sleep(0.05)
-            print(''.join(self.grid))
+            # ###### Visualize the maze solving ######
+            # time.sleep(0.05)
+            # print(''.join(self.grid))
             #######################################
 
-            node = t[0]; cost = t[1]
+            node, cost = t
             self.frontierSet.remove(node)
             self.explored.add(node)    # Add to Explored set
             if node in self.dotPositions:
                 self.dotCount -= 1     # If a dot position is found, decrement the count
                 if self.dotCount == 0: # Goal test - if all the dot positions have been found
                     return self.grid, cost
-            x = node[1]; y = node[0]; row = self.grid[y] # Get coordinates an corresponding row
+            y, x = node; row = self.grid[y] # Get coordinates an corresponding row
             if self.grid[y][x] != 'P':                   # Don't replace the 'P'
                 self.grid[y] = row[:x] + '.' + row[x+1:] # Mark the visted node with a '.'
             neighbors = self.getValidNeighbors(node)
-            for n in neighbors:        # Put all the valid neighbors in the frontier
-                frontier.put((n, cost+1))        # Put in the incremented cost
+            for n in neighbors:           # Put all the valid neighbors in the frontier
+                frontier.put((n, cost+1)) # Put in the incremented cost
                 self.frontierSet.add(n)
 
 if len(sys.argv) < 2:
     raise Exception("Need two arguments: script.py maze.txt")
 maze_fname = sys.argv[1]
 
-# maze_fname = "TestMaze.txt"
+maze_fname = "TestMaze.txt"
 # maze_fname = "MediumMaze.txt"
 # maze_fname = "BigMaze.txt"
 # maze_fname = "TinySearch.txt"
@@ -93,13 +100,10 @@ maze_fname = sys.argv[1]
 with open(maze_fname) as mFile:
     grid = mFile.readlines() # 2D Matrix of rows(string per line) and cols(character per string)
 
-# for row in grid:
-#     print(row)
-# print(grid)
-
 maze = MazeSearch(grid)
 newGrid, cost = maze.solve(False)
 newMaze = ''.join(newGrid)
+
 print(newMaze)
 print ("The path cost is "+ str(cost) + " steps")
 
