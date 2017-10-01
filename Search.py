949c2997640f137c6ab757326e8e1e61b34f94ca
upandey3 <utki.nba@gmail.com>
2017-09-30 21:55:59 -0500

    Finish 1.2 with A* and Greedy done

--- Search.py
+++ Search.py
@@ -1,6 +1,12 @@
 import sys
-from queue import *
+from queue import Queue, LifoQueue, PriorityQueue
 import time
+import math
+
+# Enum for Search Algorithms
+class Algo:
+    BFS, DFS, A_STAR, GREEDY = range(4)
+
 class MazeSearch:
 
     # Constructor
@@ -28,9 +34,10 @@ class MazeSearch:
     # Gets the Manhattan Distance between two coordinates
     def mDistance(self, node1, node2):
         assert(isinstance(node1, tuple) and isinstance(node2, tuple))
-        x1 = node1[1]; y1 = node1[0]
-        x2 = node2[1]; y2 = node2[0]
+        y1, x1 = node1
+        y2, x2 = node2
         return abs(x2 - x1) + abs(y2 - y1)
+        # return math.sqrt((y2 - y1)**2 + (x2 - x1)**2) # Euclidean distance
 
     # Check whether a node has not been visited yet
     def isUnexplored(self, node):
@@ -57,10 +64,13 @@ class MazeSearch:
         return l
 
     # Solves a maze based on a given algorithm and returns (grid, path cost)
-    def solve(self, bfs = True):
+    def solve(self, algo):
 
-        # Intialize the data structure depending on the algorithm
-        frontier = Queue(maxsize=0) if(bfs) else LifoQueue(maxsize=0)
+        if algo is Algo.A_STAR or algo is Algo.GREEDY:
+            return self.solveAG(algo)
+
+        # Intialize the data structure depending on the Algorithm
+        frontier = Queue(maxsize=0) if(algo is Algo.BFS) else LifoQueue(maxsize=0)
         frontier.put((self.start, 0))   # Frontier: ((x, y), path cost) : tuple(tuple(x, y), int)
         self.frontierSet = {self.start} # Creating a set of all the nodes that are in the frontier for finding purposes
         self.explored = set()           # Set of all the nodes that have been already explored
@@ -68,11 +78,10 @@ class MazeSearch:
         while not frontier.empty():
             t = frontier.get()      # Remove from frontier
 
-            # ###### Visualize the maze solving ######
-            # time.sleep(0.05)
-            # print(''.join(self.grid))
-            #######################################
-
+            ##### Visualize the maze solving ######
+            time.sleep(0.05)
+            print(''.join(self.grid))
+            ######################################
             node, cost = t
             self.frontierSet.remove(node)
             self.explored.add(node)    # Add to Explored set
@@ -88,24 +97,62 @@ class MazeSearch:
                 frontier.put((n, cost+1)) # Put in the incremented cost
                 self.frontierSet.add(n)
 
-if len(sys.argv) < 2:
-    raise Exception("Need two arguments: script.py maze.txt")
-maze_fname = sys.argv[1]
+    # Helper function to solve the maze using A* and
+    def solveAG(self, algo):
 
-maze_fname = "TestMaze.txt"
-# maze_fname = "MediumMaze.txt"
-# maze_fname = "BigMaze.txt"
-# maze_fname = "TinySearch.txt"
+        frontier = PriorityQueue() # Intialize the a priority queue
+        dot = (list(self.getDotPositions()))[0]
+        score = self.mDistance(self.start, dot) # Manhattan distance for Greedy, MDist + Cost(0) for A*
 
-with open(maze_fname) as mFile:
-    grid = mFile.readlines() # 2D Matrix of rows(string per line) and cols(character per string)
+        frontier.put((score, self.start, 0))   # Frontier: ((x, y), path cost) : tuple(tuple(x, y), int)
+        self.frontierSet = {self.start} # Creating a set of all the nodes that are in the frontier for finding purposes
+        self.explored = set()           # Set of all the nodes that have been already explored
 
-maze = MazeSearch(grid)
-newGrid, cost = maze.solve(False)
-newMaze = ''.join(newGrid)
+        while not frontier.empty():
+            t = frontier.get()      # Remove from frontier
 
-print(newMaze)
-print ("The path cost is "+ str(cost) + " steps")
+            ##### Visualize the maze solving ######
+            time.sleep(0.05)
+            print(''.join(self.grid))
+            ######################################
+            _, node, cost = t
+            self.frontierSet.remove(node)
+            self.explored.add(node)    # Add to Explored set
+            if node in self.dotPositions:
+                self.dotCount -= 1     # If a dot position is found, decrement the count
+                if self.dotCount == 0: # Goal test - if all the dot positions have been found
+                    return self.grid, cost
+            y, x = node; row = self.grid[y] # Get coordinates an corresponding row
+            if self.grid[y][x] != 'P':                   # Don't replace the 'P'
+                self.grid[y] = row[:x] + '.' + row[x+1:] # Mark the visted node with a '.'
+            neighbors = self.getValidNeighbors(node)
+            for n in neighbors:           # Put all the valid neighbors in the frontier
+                if algo is Algo.A_STAR: # if A*, score is f = g(cost) + h(mDist)
+                    score = cost+1 + self.mDistance(n, dot)
+                else: # if Greedy, score is Manhattan distance
+                    score = self.mDistance(n, dot)
+                frontier.put((score, n, cost+1)) # Put in the incremented cost
+                self.frontierSet.add(n)
 
-with open("output.text", "w") as oFile:
-    oFile.write(newMaze)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        raise Exception("Need two arguments: script.py maze.txt")
+    maze_fname = sys.argv[1]
+
+    with open(maze_fname) as mFile:
+        grid = mFile.readlines() # 2D Matrix of rows(string per line) and cols(character per string)
+
+    maze = MazeSearch(grid)
+    start = time.time()
+    newGrid, cost = maze.solve(Algo.A_STAR)
+    end = time.time()
+    newMaze = ''.join(newGrid)
+    print(newMaze)
+    print ("The path cost is "+ str(cost) + " steps")
+    print ("Elapsed time: " + str(end - start))
+
+    with open("output.text", "w") as oFile:
+        oFile.write(newMaze)
